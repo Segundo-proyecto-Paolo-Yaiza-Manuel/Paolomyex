d51de3d7958664341d3845457d7e52b1db9f6e47
y4izus <yaiza.garcia.mm@gmail.com>
2017-09-20 20:20:08 +0200

    Bitfinex added taking the BTC value with ajax

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,15 +1,19 @@
 const gdaxAPI = new GDAXHandler("https://api-public.sandbox.gdax.com")
+const bitfinexAPI = new BitfinexHandler("https://api.bitfinex.com")
 
 $(document).ready( () => {
 
   $('#select-currency').on('change', (e) => {
     const market = $('#select-currency').val()
     gdaxAPI.getTicker(market)
-      .then(ticker => $('#market-values').append(displayMarketValue(`GDAX`, market, ticker)))
+      .then(ticker => $('#market-values').append(displayMarketValue(`In GDAX the BTC value is ${ticker.price}`)))
+      .catch(err => console.log(err))
+    bitfinexAPI.getTicker(market)
+      .then(ticker => $('#market-values').append(displayMarketValue(`In Bitfinex the BTC value is ${ticker[6]}`)))
       .catch(err => console.log(err))
   })
 })
 
-function displayMarketValue(exchange, market, ticker){
-  return $('<p>').text(`In ${exchange} the ${market} value is ${ticker.price}`)
+function displayMarketValue(infoMarket){
+  return $('<p>').text(infoMarket)
 }
