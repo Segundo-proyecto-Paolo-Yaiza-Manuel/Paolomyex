99ca8404ec92ea7c4bdfbc921e6963df9a65317e
y4izus <yaiza.garcia.mm@gmail.com>
2017-09-21 20:39:22 +0200

    Comunica el front y back para iniciar el arbitraje

--- controllers/WalletController.js
+++ controllers/WalletController.js
@@ -2,31 +2,32 @@ const Wallet = require('../models/Wallet')
 
 module.exports = {
 
-selectExGet: (req, res) => {
-  res.render('wallets/create')
-},
+  selectExGet: (req, res) => {
+    res.render('wallets/create')
+  },
 
-createWalletGet: (req, res) => {
-  res.render('wallets/show')
-},
+  createWalletGet: (req, res) => {
+    res.render('wallets/show')
+  },
 
-createWalletPost: (req, res) => {
-  const GDAXClicked = req.body.exchangeGDAX
-  if(GDAXClicked == 'on')
-    new Wallet({
-      exchangeSite: 'GDAX'
-    })
-    .save()
-    .then(()=> console.log('WALLET CREADA'))
+  createWalletPost: (req, res) => {
+    const GDAXClicked = req.body.exchangeGDAX
+    if(GDAXClicked == 'on')
+      new Wallet({
+        exchangeSite: 'GDAX'
+      })
+      .save()
+      .then(()=> console.log('WALLET CREADA'))
 
-  res.redirect('/wallets/selectexchange')
+    res.redirect('/wallets/selectexchange')
+  },
 
+  initArbitrageGet:(req, res) => {
+    res.render('wallets/new')
+  },
 
-}
-
-
-}
-
-function createWalletGDAX(){
-
+  initArbitragePost:(req, res) => {
+    console.log('INICIA EL ARBITRAJE');
+    console.log(req.body);
+  }
 }
