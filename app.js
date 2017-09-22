570d6766c2ac6e69351b14b72d50889d00185916
y4izus <yaiza.garcia.mm@gmail.com>
2017-09-22 13:18:57 +0200

    El bot ejecuta un console.log con los usuarios en arbitraje cada minuto

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+var schedule = require('node-schedule')
 const app = require('express')()
+const User = require("./models/User")
 
 require('./config/passport')()
 require('./config/express')(app)
@@ -14,6 +16,15 @@ app.use('/', index)
 app.use('/card', cardRoutes)
 app.use('/users', usersRoutes)
 app.use('/wallets', walletsRoutes)
+
+
+var bot = schedule.scheduleJob('*/1 * * * *', function(){
+    User.find({ inArbitrage: true })
+    .then(usersInArbitrage => usersInArbitrage.forEach( user => {
+      console.log(`${user.firstname} is doing arbitrage with ${user.money}$`)
+    }))
+  console.log('===========BOT==========')
+})
 
 require('./config/error-handler')(app)
 module.exports = app
